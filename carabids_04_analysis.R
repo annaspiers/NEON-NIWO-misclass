ce153f3bd0d6e3d5ee80bcb613a5402484dab98f
Anna Spiers <anna.spiers@colorado.edu>
2020-02-27 09:54:54 -0700

    push analysis edits

--- carabids_04_analysis.R
+++ carabids_04_analysis.R
@@ -2,3 +2,11 @@
 
 library(arm)
 
+dat <- lmer
+
+# Try modeling
+
+fit_abund <- glmer(formula = sp_abund ~ nlcdClass + para_sciname + (1|plotID) , 
+    family = poisson(link="log"),
+      data = dat)
+display(fit_abund)
