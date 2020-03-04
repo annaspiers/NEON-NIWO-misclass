e3ed2c7f5ebcde33cd9f4249b412805cb30af4d2
Anna Spiers <anna.spiers@colorado.edu>
2020-03-04 09:14:42 -0700

    separate carabid EDA scripts

---
+++ carabids_03_EDA_carabid-modeling_explo.R
@@ -0,0 +1,92 @@
+# This script explores the differences in species ED between the parataxonomist, expert taxonomist, and barcode datasets on NEON ground beetles, then visualizes the data
+# Para vs expert comparison written by Max
+
+library(dplyr)
+#library(stringr) #word()
+#library(ggplot2)
+#library(forcats) #fct_reorder
+#library(arm) #display
+
+# Load carabid data
+load(file="data_derived/carabids_NIWO.Rdata")
+list2env(carabid_abund, .GlobalEnv)  
+
+
+# Join para and expert data
+expert <- bet_expertTaxonomistIDProcessed %>%
+            mutate(expert_sciname = paste(genus, specificEpithet)) %>%
+            as_tibble
+para <- bet_parataxonomistID %>%
+            mutate(para_sciname = scientificName) %>%
+            select(individualID, plotID, trapID, collectDate, morphospeciesID, taxonRank, para_sciname) %>%
+            as_tibble
+taxon_df <- left_join(para, expert)
+
+# Visualize those species with 100% match between para and expert IDs and with n>20
+select_spp <- taxon_df %>%
+  mutate(same_species = para_sciname == expert_sciname) %>%
+  group_by(para_sciname) %>%
+  summarize(pct_correct = mean(same_species, na.rm = TRUE), 
+            tot_correct = sum(same_species, na.rm = TRUE), #number of individuals for this species where para = expert where expert exists
+            n = n()) %>% #number of individuals with para_sciname
+  filter(pct_correct == 1, n > 20) %>%
+  pull(para_sciname)
+
+
+
+### Modeling EDA
+# What variables might be related to carabid abundance?
+
+carabid_df <- taxon_df %>% 
+  filter(para_sciname %in% select_spp) %>%
+  left_join(distinct(bet_fielddata %>%
+              select(plotID, nlcdClass, decimalLatitude, decimalLongitude, elevation))) %>%
+  mutate(year = lubridate::year(collectDate), 
+         month = lubridate::month(collectDate), 
+         day = lubridate::day(collectDate)) %>%
+       mutate_at(c("morphospeciesID", "taxonRank", "para_sciname", "expert_sciname", "plotID", "trapID", "nlcdClass"), funs(factor(.)))
+
+# Group data by plot and species and summarize by species abundance
+model_test_abund <- carabid_df %>%
+  group_by(plotID, trapID, para_sciname, nlcdClass) %>% #para_sciname
+  summarize(sp_abund = n() ) %>%
+  arrange(plotID) %>%
+  data.frame()
+
+model_test_comp <- carabid_df %>%
+  group_by(plotID, nlcdClass) %>%
+  summarize(sp_comp = n_distinct(para_sciname, na.rm = TRUE) ) %>%
+  arrange(plotID) %>%
+  data.frame()
+# AIS later, try grouping by collection date and trapID
+
+# Is habitat type a good predictor for species abundance?
+# Constant
+fit_abund_0 <- glm(formula = sp_abund ~ 1, 
+    family = poisson(link="log"),
+      data = model_test_abund)
+# With habitat type
+fit_abund_1 <- glm(formula = sp_abund ~ nlcdClass, 
+    family = poisson(link="log"),
+      data = model_test_abund)
+fit_abund_2 <- glmer(formula = sp_abund ~ nlcdClass + para_sciname + (1|plotID) , 
+    family = poisson(link="log"),
+      data = model_test_abund)
+display(fit_abund_0)
+display(fit_abund_1)
+display(fit_abund_2)
+anova(fit_abund_0, fit_abund_1, fit_abund_2)
+
+# Is habitat type a good predictor for species composition?
+# Constant
+fit_comp_0 <- glm(formula = sp_comp ~ 1, 
+    family = poisson(link="log"),
+      data = model_test_comp)
+# With habitat type
+fit_comp_1 <- glm(formula = sp_comp ~ nlcdClass, 
+    family = poisson(link="log"),
+      data = model_test_comp)
+display(fit_comp_0)
+display(fit_comp_1)
+
+
--- carabids_03_EDA_carabid-species_explo.R
+++ carabids_03_EDA_carabid-species_explo.R
@@ -228,67 +228,6 @@ bet_parataxonomistID %>%
 # All species are classified as native
 
 
-### Modeling EDA
-# What variables might be related to carabid abundance?
-
-carabid_df <- taxon_df %>% 
-  full_join(bet_parataxonomistID %>% 
-    select(individualID, plotID , trapID , collectDate)) %>% 
-  filter(para_sciname %in% select_spp) %>%
-  left_join(distinct(bet_fielddata %>%
-              select(plotID, nlcdClass, decimalLatitude, decimalLongitude, elevation))) %>%
-  mutate(year = lubridate::year(collectDate), 
-         month = lubridate::month(collectDate), 
-         day = lubridate::day(collectDate)) %>%
-       mutate_at(c("morphospeciesID", "taxonRank", "para_sciname", "expert_sciname", "bc_sciname", "plotID", "trapID", "nlcdClass"), funs(factor(.)))
-
-# Group data by plot and species and summarize by species abundance
-model_test_abund <- carabid_df %>%
-  group_by(plotID, trapID, para_sciname, nlcdClass) %>% #para_sciname
-  summarize(sp_abund = n() ) %>%
-  arrange(plotID) %>%
-  data.frame()
-
-model_test_comp <- carabid_df %>%
-  group_by(plotID, nlcdClass) %>%
-  summarize(sp_comp = n_distinct(para_sciname, na.rm = TRUE) ) %>%
-  arrange(plotID) %>%
-  data.frame()
-# AIS later, try grouping by collection date and trapID
-
-# Is habitat type a good predictor for species abundance?
-# Constant
-fit_abund_0 <- glm(formula = sp_abund ~ 1, 
-    family = poisson(link="log"),
-      data = model_test_abund)
-# With habitat type
-fit_abund_1 <- glm(formula = sp_abund ~ nlcdClass, 
-    family = poisson(link="log"),
-      data = model_test_abund)
-fit_abund_2 <- glmer(formula = sp_abund ~ nlcdClass + para_sciname + (1|plotID) , 
-    family = poisson(link="log"),
-      data = model_test_abund)
-display(fit_abund_0)
-display(fit_abund_1)
-display(fit_abund_2)
-anova(fit_abund_0, fit_abund_1, fit_abund_2)
-
-# Is habitat type a good predictor for species composition?
-# Constant
-fit_comp_0 <- glm(formula = sp_comp ~ 1, 
-    family = poisson(link="log"),
-      data = model_test_comp)
-# With habitat type
-fit_comp_1 <- glm(formula = sp_comp ~ nlcdClass, 
-    family = poisson(link="log"),
-      data = model_test_comp)
-display(fit_comp_0)
-display(fit_comp_1)
-
-
-
-
-
 
 ### SCRATCH ###
 # # How to decide on the final species ID to use?
