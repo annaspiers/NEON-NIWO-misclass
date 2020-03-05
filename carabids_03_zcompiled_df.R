4a98b8f4f1006bf36baaf51e32b35d4424f55e80
Anna Spiers <anna.spiers@colorado.edu>
2020-03-04 22:06:05 -0700

    add rows for absent species

--- carabids_03_zcompiled_df.R
+++ carabids_03_zcompiled_df.R
@@ -38,15 +38,43 @@ model_df_by_ind <- bet_parataxonomistID %>%
     left_join(def.extr.geo.os(data = carabid_abund$bet_fielddata, 'namedLocation', locOnly=T) %>% #join plot soil type
             dplyr::select("plotID" = Value.for.Plot.ID, "soilOrder" = api.soilTypeOrder))
 
-# each row is a para_sciname in a sample
-model_df_by_sample <- model_df_by_ind %>%
+# each row is a para_sciname in a sample with 0 abundance rows included
+all_samples_df <- model_df_by_ind %>%
     group_by(siteID, plotID, trapID, sampleID, collectDate, col_year, col_month, col_day, dayofyear, trappingDays, decimalLatitude, decimalLongitude, elevation, nlcdClass, soilOrder, para_sciname) %>%
-    summarize(sp_abund=n())
+  summarize(sp_abund=n())
+
+# add absences
+all_samps_na_sp_df <- all_samples_df %>%
+  mutate(para_sciname = NA) %>%
+  dplyr::select(-sp_abund)
+for(i in 1:length(select_spp)) {
+    df_i <- all_samples_df %>%
+      mutate(para_sciname = NA) %>%
+      dplyr::select(-sp_abund)
+    df_i$para_sciname <- select_spp[i]
+    all_samps_na_sp_df <- rbind(all_samps_na_sp_df, df_i)
+    rm(df_i)
+}
+# remove initial df chunk with para_sciname values as NA
+all_samps_all_sp_df <- all_samps_na_sp_df %>% filter(is.na(para_sciname)==FALSE) 
+
+# Join with df organized by collectDate  in trap in plot and summarize for sp_abund count
+model_df_by_sample <- all_samps_all_sp_df %>% 
+  left_join(all_samples_df) %>%
+  arrange(collectDate)
+
+# Convert sp_abund NAs to 0s
+model_df_by_sample$sp_abund[is.na(model_df_by_sample$sp_abund)] <- 0
+unique(model_df_by_sample$sp_abund)
+
+
+
+
 
 
 ### Save df ###
   
-write.csv(model_df_by_ind, file="data_derived/model_df_by_individual_beetle.csv") #df by individuals
-write.csv(model_df_by_sample, file="data_derived/model_df_by_species_in_sample.csv") #df by para_sciname in sample
+write.csv(model_df_by_ind, file="data_derived/model_df_by_individual_beetle.csv", row.names = FALSE) #df by individuals
+write.csv(model_df_by_sample, file="data_derived/model_df_by_species_in_sample.csv", row.names = FALSE) #df by para_sciname in sample
 
 
