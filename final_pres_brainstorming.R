402dcf9b98d07ae27ae09097ee9ef4f943573f02
Anna Spiers <anna.spiers@colorado.edu>
2020-04-09 16:10:52 -0600

    brainstorming from class on April 9

---
+++ final_pres_brainstorming.R
@@ -0,0 +1,28 @@
+
+# Brainstorming for forecasting final presentation
+
+# Premise
+
+
+# What were our questions
+# Hypothesized strong spatial/microhabitat effects
+# Predict when is peak abundance for each species in 2019. Assess based on timing and magnitude of peak. Area under curve for total seasonal abundance
+# Here, we focus on two species. Carabus taedatus (more in tundra) and Cymindis unicolor (more in fores) are the only two species present in both nlcdClass
+
+
+# EDA
+# Beetles...
+# 
+# The literature supported certain explanatory variables, but we didn't some of these to explain much variance in our models.
+# 
+#  Predictor variables...
+# How did we calculate LAI (and other AOP data products) for each trap?
+    # How did we decide on what size buffer to use for these calculations?
+
+
+# Model 
+# compare GAMM and GLMM?
+
+
+# Results and conclusions
+# Did not find strong spatial effect, but did find noticeable temporal effects
