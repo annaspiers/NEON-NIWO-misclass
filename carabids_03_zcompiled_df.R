9aa1f568286b05e47d993a1b68033f2b14c192da
Anna Spiers <anna.spiers@colorado.edu>
2020-04-09 15:01:53 -0600

    streamlined dataframe construction

--- carabids_03_zcompiled_df.R
+++ carabids_03_zcompiled_df.R
@@ -20,12 +20,13 @@ list2env(carabid_abund, .GlobalEnv)
 #load("data_derived/rad_net_NIWO.Rdata")
 #load("data_derived/rad_short_dir_diff_NIWO.Rdata")
 load("data_derived/trap_LAI_sp.Rdata")
+load("data_derived/trap_CHM_sp.Rdata")
 
-### Combine into one df ###
+# Select 7 most abundant spp.
 select_spp <- c("Amara alpina", "Amara quenseli", "Calathus advena", "Carabus taedatus", "Cymindis unicolor", "Harpalus nigritarsis", "Pterostichus restrictus")
 
 
-# I Create df where each row is an individual beetle ------------------------
+# I. Create df where each row is an individual beetle ------------------------
 
 model_df_by_ind <- bet_parataxonomistID %>%
     filter(scientificName %in% select_spp) %>% #filter to selected 7 species
@@ -44,107 +45,43 @@ model_df_by_ind <- bet_parataxonomistID %>%
 
 
 
-# II Create df where each row is a species in a trap from a sing --------
+# II. Create df where each row is a species in a trap from a sing --------
 
-#AIS edit this to use complete()
+# Initialize df organized by each species by collectionDate by trap by plot
+model_df_by_sample <- model_df_by_ind %>%
+  distinct(plotID, trapID, collectDate, para_sciname) %>%
+  arrange(plotID, trapID, collectDate, para_sciname) %>%
+  complete(plotID, trapID, collectDate, para_sciname) %>%
+  left_join(model_df_by_ind %>% 
+              group_by(plotID,trapID,collectDate,para_sciname) %>%
+              summarize(sp_abund = n()) ) %>% # add in non-zero species abundance
+  mutate(sp_abund = ifelse(is.na(sp_abund),0,sp_abund)) %>% # turn NA abundance into 0
+  left_join(model_df_by_ind %>% 
+              dplyr::select(siteID,plotID,plot_lat=decimalLatitude,plot_long=decimalLongitude,elev=elevation,nlcdClass,soilOrder) %>%
+              distinct()) %>% # attach useful spatial variables from model_df_by_ind
+  left_join(model_df_by_ind %>% 
+              dplyr::select(collectDate,col_year,col_month,col_day,DOY=dayofyear) %>%
+              distinct()) %>% # attach useful temporal variables from model_df_by_ind
+  left_join(model_df_by_ind %>%
+           distinct(col_year, collectDate) %>%
+           group_by(col_year) %>%
+           mutate(col_index = 1:n())) %>% #index the collection date within each year
+  filter(!(plotID == "NIWO_004" & col_year == 2018), # Remove rows for plot 4 in 2018
+         !(plotID == "NIWO_013" & (col_year==2015 | col_year==2016 | col_year==2017 ))) # Remove rows for plot 13 in 2015-2017
 
-# How many rows should this df have?
-length(unique(model_df_by_ind$collectDate)) #24 unique collectDates
-# 960 rows per species = (24 collectDates)*(4 traps)*(10 plots)
-
-# initialize 960 row df 
-#initialize df with plot 13 in 2018
-dates2018 <- c("2018-06-05","2018-06-19","2018-07-03","2018-07-17","2018-07-31","2018-08-14","2018-08-28")
-all_samples_df <- data.frame(plotID = rep("NIWO_013", 28), 
-                             trapID = c(rep("N",7),rep("E",7),rep("S",7),rep("W",7)), 
-                             collectDate = rep(as.factor(dates2018), 4) )
-# then add on each unique collectDate by trap by plot
-for ( i in 1:(length(unique(model_df_by_ind$plotID))-1) ) {
-  for ( j in 1:length(unique(model_df_by_ind$trapID)) ) {
-    plot_i_trap_j_df <- data.frame(plotID = rep(unique(model_df_by_ind$plotID)[i], 24), 
-                                   trapID = rep(unique(model_df_by_ind$trapID)[j], 24), 
-                                   collectDate = rep(unique(as.factor(model_df_by_ind$collectDate)), 1) )
-    all_samples_df <- rbind(all_samples_df, plot_i_trap_j_df)
-  }
-}
-# remove rows for  plot 4 in 2018
-all_samples_df <- all_samples_df %>% 
-  filter( !(plotID == "NIWO_004" & collectDate %in% dates2018) )
-nrow(all_samples_df)# Now we have 960 rows
-
-# Create sampleID column
-all_samples_df <- all_samples_df %>%
-  mutate(sampleID = paste(plotID, trapID, gsub("-","",collectDate), sep="."))
-length(unique(all_samples_df$sampleID)) #check that there are 960 unique sampleIDs
-
-# Add on descriptor columns (not species and abundance)
-descriptors_df <- model_df_by_ind %>%
-    group_by(siteID, plotID, decimalLatitude, decimalLongitude, elevation, nlcdClass, soilOrder) %>%
-  summarize()
-all_samp_desc_df <- all_samples_df %>%
-  mutate(col_year = lubridate::year(collectDate),
-         col_month = lubridate::month(collectDate),
-         col_day = lubridate::day(collectDate),
-         dayofyear = as.numeric(strftime(collectDate, format = "%j"))) %>%
-  mutate_at(c("plotID", "trapID"), funs(as.character(.))) %>%
-  left_join(descriptors_df) %>%
-  arrange(collectDate)
-nrow(all_samp_desc_df %>% filter(is.na(siteID)==TRUE))
-
-# Multiply out df for number of species
-temp_df <- all_samp_desc_df %>% 
-  mutate(para_sciname = NA) 
-for(i in 1:length(select_spp)) {
-    df_i <- all_samp_desc_df %>% 
-      mutate(para_sciname = select_spp[i]) 
-    temp_df <- rbind(temp_df, df_i)
-    rm(df_i)
-}
-# remove initial df chunk with para_sciname values as NA
-temp_df <- temp_df %>% filter(is.na(para_sciname)==FALSE) 
-nrow(temp_df) #df is 960*(7 species)=6720 rows
-
-# Create df summarizing all samples that have >0 abundance
-all_samps_w_abund_df <- model_df_by_ind %>%
-    group_by(sampleID, para_sciname) %>%
-  summarize(sp_abund=n())
-
-# Merge temp_df and all_samps_w_abund_df so each possible species sample is joined with its abundance
-model_df_by_sample <- temp_df %>%
-  left_join(all_samps_w_abund_df)
-nrow(model_df_by_sample) #6270 rows
-
-# Convert sp_abund NAs to 0s
-model_df_by_sample$sp_abund[is.na(model_df_by_sample$sp_abund)] <- 0
-unique(model_df_by_sample$sp_abund) #no NAs
-
-# Index collection date within each year
-secondary_period_df <- model_df_by_sample %>%
-    distinct(col_year, collectDate) %>%
-    filter(!is.na(col_year)) %>%
-    group_by(col_year) %>%
-    mutate(col_index = 1:n()) 
-model_df_by_sample <- model_df_by_sample %>%
-  left_join(secondary_period_df)
   
-# Add in more predictor variables -----------------------------------------
-
-#LAI
-trap_LAI_sp_edited <- trap_LAI_sp@data %>%
-  separate(locationNa, into = c('plotID','junk','junk2','trapID'), sep="\\.", remove=FALSE) %>%
-  dplyr::select(-c(locationNa, junk, junk2), trap.Easting=x, trap.Northing=y)
+# III. Add in more predictor variables -----------------------------------------
 
 model_df_by_sample <- model_df_by_sample %>%
-  left_join(trap_LAI_sp_edited)
+  left_join(trap_LAI_sp@data %>%
+              separate(locationNa, into = c('plotID','junk','junk2','trapID'), sep="\\.", remove=FALSE) %>%
+              dplyr::select(-c(locationNa, junk, junk2), trap_easting=x, trap_northing=y)) %>% #add LAI
+  left_join(trap_CHM_sp@data %>%
+              rename(trap_CHM = trap_CH, trap_easting=trap.Easting, trap_northing=trap.Northing)) #add CHM
 
-# CHM
-load("data_derived/trap_CHM_sp.Rdata")
-model_df_by_sample <- model_df_by_sample %>%
-  left_join(trap_CHM_sp@data)
-model_df_by_sample <- model_df_by_sample %>%
-  rename(trap_CHM = trap_CH)
 
-### Save df ###
+
+# Save dataframes to csv --------------------------------------------------
   
 write.csv(model_df_by_ind, file="data_derived/model_df_by_individual_beetle.csv") #df by individuals
 write.csv(model_df_by_sample, file="data_derived/model_df_by_species_in_sample.csv") #df by para_sciname in sample
