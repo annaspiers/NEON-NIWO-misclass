770cfdd306ea084117a85410540f2661a7baeabe
Anna Spiers <anna.spiers@colorado.edu>
2020-04-13 10:57:54 -0600

    dynamic multispecies model

--- occupancy/08_neon_dynamic_multisp.R
+++ occupancy/08_neon_dynamic_multisp.R
@@ -1,27 +1,35 @@
 # This script is models the dynamic occupancy of NEON carabid speciesat Niwot Ridge 2015-2018. Data include 7 most abundant and accurately-identified species
 
 library(jagsUI)
-library(tidyverse)
+library(dplyr)
 library(reshape2)
 
 
 # Read in and clean data  -------------------------------------------------------
 
-raw_dat <- read.csv("data_derived/model_df_by_species_in_sample.csv", header = TRUE)
-
-clean_dat <- raw_dat %>% 
+dat <- read.csv("data_derived/model_df_by_species_in_sample.csv", header = TRUE) %>%
     mutate(occ = ifelse(sp_abund > 0, 1, 0),
            plot_trap = paste(plotID,trapID, sep=""))
 
+
 # Create detection array --------------------------------------------------
 
-y <- dat_2018 %>%
-    #distinct(plot_trap, col_index, occ) %>%
-    reshape2::acast(plot_trap ~ para_sciname ~ col_index, 
+# dimensions of y: 
+# 1. plot_trap
+# 2. species
+# 3. number sampling occasions per year
+# 4. number of years
+y <- dat %>%
+    reshape2::acast(plot_trap ~ para_sciname ~ col_index ~ col_year, 
                     value.var = "occ")
 str(y) # dim is [1:40, 1:7, 1:7] 40 traps, 7 species, 7 surveys
-#image(t(y[,,3]))
-#table(y[,,3])
+image(t(y[,,1,1])) #AIS why is top row blank?
+image(t(y[,,2,1]))
+image(t(y[,,3,1]))
+image(t(y[,,4,1]))
+image(t(y[,,5,1]))
+#table(y[,,1,1])
+#table(y[,,5,1])
 
 
 # Fit model ---------------------------------------------------------------
@@ -29,31 +37,40 @@ str(y) # dim is [1:40, 1:7, 1:7] 40 traps, 7 species, 7 surveys
 str(JAGSdata <- list(y = y, 
                      ntrap = dim(y)[1], 
                      nspec = dim(y)[2], 
-                     nsurv = dim (y)[3])) #bundle data
-zst <- apply(y, c(1,2), max, na.rm = TRUE) #AIS what does zst mean, intuitively? z start?
-zst[zst == "-Inf"] <- NA #AIS why would a value of -Inf be generated?
+                     nsurv = dim(y)[3],
+                     nyear = dim(y)[4])) 
+zst <- apply(y, c(1,2,4), max, na.rm = TRUE) # closure across surveys. varies by trap, species, and year.
+zst[zst == "-Inf"] <- NA #this happens where a plot isn't sampled across years 
+# AIS how does JAGS handle inits (i.e. compared to handling data)?
 JAGSinits <- function(){list(z = zst) } 
-JAGSparams <- c("psi", "p") #params monitored
-nc <- 3 #MCMC chains
-ni <- 2500 #MCMC iterations
-nb <- 500 #MCMC burnin
-nt <- 2  #MCMC thin
+JAGSparams <- c("psi", "p", "phi", "gamma", "n.occ", "growth", "turnover") 
+nc <- 4     #MCMC chains 
+ni <- 10000  #MCMC iterations
+nb <- 1800  #MCMC burnin
+nt <- 2     #MCMC thin
 
 # JAGS model
-jags_out <- jags(data = JAGSdata,
+JAGSout <- jags(data = JAGSdata,
                  inits = JAGSinits,
                  parameters.to.save = JAGSparams,
-                 model.file = "occupancy/07_neon_static_multisp_JAGS.txt", 
+                 model.file = "occupancy/08_neon_dynamic_multisp_JAGS.txt", 
                  n.chains = nc,
                  n.iter = ni,
                  n.burnin = nb,
                  n.thin = nt)
+# with autojags, I could see that growth[2,2], gamma[7,1],gamma[1,1], gamma[1,3], gamma[2,3], gamma had trouble converging
+# AIS species 2 and 7 were not present at beginning of survey 92015 and 2015-2016, respectively) - see output photo
 
-print(jags_out, dig=2)
-names(jags_out)
-plot(jags_out) 
-# AIS 
+print(JAGSout, dig=2)
+names(JAGSout) #JAGout$mean, see growth[2,2] and growth[7,3] - too big!
+#JAGSout$mean - most p values are larger than corresponding psi. Is p conditional on phi?
+plot(JAGSout) #nonconverged: psi[2,1], psi[7,1], psi[7,2]. many of the phi's look wonky. most of the gamma's look wonky too. growth[2,2], growth[7,2], growth[7,3] DNE. bimodal turnover[7,1]
+#AIS what are the horizontal lines through the trace plots?
+View(JAGSout)
+whiskerplot(JAGSout, c("psi"), zeroline=T)
+pp.check(JAGSout, observed, simulated, xlab=NULL, ylab=NULL, main=NULL)
 
-# return in the morning - make a 08 script that is dynamic, multispecies. or try incorporating all samples, even rare ones. Also, try incorporating covariates!!
-# AIS this model assumes closure within a season. Allow z to vary by survey to incorporate seasonality
-# Next, allow parameters to vary by sampling occassion (bootom of bpa ch 14 p.456)
+
+# IN THE MORNING - how to visualize these results nicely?
+
+# AIS Next, allow parameters to vary by sampling occassion (bootom of bpa ch 14 p.456)
