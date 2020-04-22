e0e4192cf6ce260b279b5862bc4bfe5cabf37208
Anna Spiers <anna.spiers@colorado.edu>
2020-04-22 11:00:22 -0600

    summarize precip accumulated 2 weeks prior to collection

--- carabids_03_EDA_precip.R
+++ carabids_03_EDA_precip.R
@@ -6,11 +6,14 @@ library(ggplot2)
 library(lubridate)
 
 # Load data
-# Load soil water content data
 load(file="data_derived/precip_NIWO.Rdata") # NEON precip 
 list2env(precip, .GlobalEnv)  
 rm(precip)
 load(file="data_derived/merged_C1-saddle_precip.Rdata") # Niwot LTER C1 and saddle precip
+merged_precip <- merged_precip %>% 
+    mutate(collectDate = as.Date(NA,format="%Y-%m-%d")) 
+model_df <- read.csv("data_derived/model_df_by_species_in_sample.csv") %>%
+    mutate(collectDate = as.Date(collectDate,format="%Y-%m-%d")) #load model df to help summarize precip data
 
 # NEON Precipitation gauge ------------------------------------------------
 
@@ -45,7 +48,7 @@ PRIPRE_30min %>%
 
 # Look at precip over time (by day)
 ggplot(merged_precip, aes(x=date, y=ppt_tot)) +
-    geom_smooth(aes(colour=local_site, alpha=.6))
+    geom_line(aes(colour=local_site, alpha=.6))
 # We see higher precip at the saddle
     
 # Monthly sums
@@ -56,4 +59,38 @@ merged_precip %>%
     ggplot() +
     geom_line(aes(x = month, y = monthly_precip, colour=local_site))
 # Wow, soo much more precip at the saddle than at C1
+
+
+# These data seem usable, so let's summarize them to be able to plug right into the model df in the 03_zcompiled script
+# Assign which collection window a precipitation date falls under, if any
+uni_collDates <- data.frame("collectDate"=unique(model_df$collectDate)) %>%
+    mutate(two_wk_int = interval(ymd(collectDate-14), ymd(collectDate)))
+for (i in 1:nrow(merged_precip)) {
+    for (j in 1:nrow(uni_collDates)) {
+        if (merged_precip$date[i] %within% uni_collDates$two_wk_int[j]) {
+            merged_precip$collectDate[i] <- uni_collDates$collectDate[j]
+        }
+    }
+}
+
+# Summarize the accumulated precip for 2 weeks prior to each collection date
+summ_precip <- merged_precip %>%
+    select(local_site, ppt_tot, collectDate) %>%
+    group_by(local_site, collectDate) %>%
+    summarise(precip_2weeks = sum(ppt_tot, na.rm = T)) %>%
+    filter(is.na(collectDate)==FALSE) %>%
+    mutate(nlcdClass = ifelse(local_site=="c1","evergreenForest","grasslandHerbaceous")) %>%
+    ungroup() %>%
+    mutate(col_year = lubridate::year(collectDate), 
+           col_month = lubridate::month(collectDate),
+           col_day = lubridate::day(collectDate))
+
+# Visualize accummulated precip for each collection date
+summ_precip %>%
+    ggplot() +
+    geom_point(aes(x = collectDate, y = precip_2weeks, colour=local_site))
+
+save(summ_precip, file="data_derived/summarized_precip.Rdata") 
+
+# In carabids_03_zcompiled script, we will assign tundra plots saddle precip data and forest plots C1 precip data
 
