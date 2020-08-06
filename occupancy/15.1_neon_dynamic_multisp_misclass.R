334a36f8de29ab4efe75aa8483adcc1dcc33cda9
Anna Spiers <anna.spiers@colorado.edu>
2020-08-06 03:59:02 +0000

    change JAGS input to numeric

--- occupancy/15.1_neon_dynamic_multisp_misclass.R
+++ occupancy/15.1_neon_dynamic_multisp_misclass.R
@@ -6,74 +6,73 @@
 # library(reshape2) 
 library(tidyr) #uncount()
 library(tibble) #rownames_to_column()
-# library(gtools) 
-# library(R2jags)
 library(dclone) #alternative: R2jags::jags.parallel
-# library(MCMCpack)
 library(MCMCvis)
 library(dplyr)
 library(ggplot2)
-# library(tidyselect)
-# library(assertthat)
 options(digits = 3)
 
 # Load NEON Niwot Ridge carabid data 
 # Load data
 # Filter for 1 year (static model)
-all_paratax_df <- readRDS("occupancy/all_paratax_df.rds") %>%
-  rename(para_morph_combo = scimorph_combo) %>%
-  filter(subsampleID != "aRw+8bWvW/wOIENap0etmXiCq6V1RZPfCkRcy1w8BLk=") 
-  #AIS ^ has more pinned than sorted
-pulled_inds <- readRDS("occupancy/pinned_df.rds") %>%
-  filter(subsampleID == "aRw+8bWvW/wOIENap0etmXiCq6V1RZPfCkRcy1w8BLk=") %>%
-  pull(individualID)
-pinned_df <- readRDS("occupancy/pinned_df.rds") %>%
-  filter(subsampleID != "aRw+8bWvW/wOIENap0etmXiCq6V1RZPfCkRcy1w8BLk=")
-expert_df <- readRDS("occupancy/expert_df.rds") %>%
-  filter(!(individualID %in% pulled_inds))
-
-all_paratax_by_ind <- all_paratax_df %>%
+all_paratax_by_ind <- readRDS("occupancy/all_paratax_df.rds") %>%
+  rename(para_morph_combo = scimorph_combo)  %>%
   uncount(individualCount) %>%
   rownames_to_column() 
 all_paratax_by_ind$exp_sciname <- NA
+expert_df <- readRDS("occupancy/expert_df.rds") 
+pinned_df <- readRDS("occupancy/pinned_df.rds") 
+expert_pinned_df <- expert_df %>%
+  rename(exp_sciname = scientificName) %>%
+  left_join(pinned_df %>% dplyr::select(individualID, subsampleID, para_morph_combo = scimorph_combo))
 
-# # Filter datasets to abundant parataxonomist species/morphospecies IDs
-# common_morpho <- all_paratax_df %>%
-#   group_by(scimorph_combo, individualCount) %>%
-#   summarize(total = sum(individualCount)) %>%
-#   arrange(-total) %>%
-#   filter(total > 10)
-# 
-# all_paratax_df <- all_paratax_df %>%
-#   filter(scimorph_combo %in% common_morpho$scimorph_combo)
-# pinned_df <- pinned_df %>%
-#   filter(scimorph_combo %in% common_morpho$scimorph_combo)
-# expert_df <- expert_df %>%
-#   left_join(pinned_df %>% select(individualID, scimorph_combo)) %>%
-#   filter(scimorph_combo %in% common_morpho$scimorph_combo)
+# Filter datasets to abundant parataxonomist species/morphospecies IDs
+common_morpho <- all_paratax_by_ind %>%
+  group_by(para_morph_combo) %>%
+  summarize(total = n()) %>%
+  arrange(-total) %>%
+  filter(total > 20)
+
+all_paratax_by_ind <- all_paratax_by_ind %>%
+  filter(para_morph_combo %in% common_morpho$para_morph_combo)
+expert_pinned_df <- expert_pinned_df %>%
+  filter(subsampleID %in% all_paratax_by_ind$subsampleID)
+
+# subsample aRw+8bWvW/wOIENap0etmXiCq6V1RZPfCkRcy1w8BLk= had 
+# 8 individuals sorted, but 8 pinned and expertly identified. 
+# This is the only subsample with less sorted individuals than expertly ID'ed
+# As a workaround, we'll remove two expertly identified inidividuals
+removed_inds <- expert_pinned_df %>%
+  filter(subsampleID == "aRw+8bWvW/wOIENap0etmXiCq6V1RZPfCkRcy1w8BLk=") %>%
+  pull(individualID) %>%
+  tail(2)
+expert_pinned_df <- expert_pinned_df %>%
+  filter(!(individualID %in% removed_inds))
+
+rm(expert_df, pinned_df, common_morpho, removed_inds)
 
 # List 1) the unique expert taxonomist ID's and 2) the parataxonomist IDs that the
 # expert taxonomist didn't use. 
 # These identifications will be the row names of theta and M
-rownames <- c(expert_df %>%   #1) the unique expert taxonomist ID's
+rownames <- c(expert_pinned_df %>%   #1) the unique expert taxonomist ID's
+                distinct(exp_sciname) %>%
+                pull(exp_sciname),
+              all_paratax_by_ind %>%   #2) the parataxonomist IDs that the expert taxonomist didn't use. 
                 distinct(scientificName) %>%
-                pull(scientificName),
-              all_paratax_df %>%   #2) the parataxonomist IDs that the expert taxonomist didn't use. 
-                distinct(scientificName) %>%
-                filter(!scientificName %in% unique(expert_df$scientificName)) %>% 
+                filter(!scientificName %in% unique(expert_pinned_df$exp_sciname)) %>% 
                 pull(scientificName) ) 
 rownames <- sort(rownames)
 
 # List 1) the unique parataxonomist and morphospecies ID's and 2) the expert taxonomist IDs that the
 # parataxonomist didn't use. 
 # These identifications will be the column names of theta and M
-extra_colnames <- c(all_paratax_df %>%   #1) the unique parataxonomist and morphospecies ID's
+extra_colnames <- c(all_paratax_by_ind %>%   #1) the unique parataxonomist and morphospecies ID's
                       distinct(para_morph_combo) %>%
                       pull(para_morph_combo),
-                    expert_df %>%   #2) the expert taxonomist IDs that the parataxonomist didn't use. 
-                      distinct(scientificName) %>%
-                      filter(!scientificName %in% unique(all_paratax_df$para_morph_combo)) %>% 
-                      pull(scientificName) ) 
+                    expert_pinned_df %>%   #2) the expert taxonomist IDs that the parataxonomist didn't use. 
+                      distinct(exp_sciname) %>%
+                      filter(!exp_sciname %in% unique(all_paratax_by_ind$para_morph_combo)) %>% 
+                      pull(exp_sciname) ) 
 extra_colnames <- sort(extra_colnames)
 extra_indices <- which(!extra_colnames %in% rownames)
 colnames <- c(rownames, extra_colnames[extra_indices])
@@ -88,11 +87,7 @@ assertthat::assert_that(all(rownames == colnames[1:length(rownames)]))
 # valid because the 5 individuals are exchangeable)
 
 # initialize counter for for-loop
-subsamps <- pinned_df %>%
-  left_join(expert_df %>% 
-              dplyr::select(individualID, exp_sciname = scientificName)) %>%
-  dplyr::select(subsampleID, exp_sciname) %>%
-  filter(!is.na(exp_sciname)) %>%
+subsamps <- expert_pinned_df %>%
   distinct(subsampleID) %>% pull(subsampleID)
 # para_new will replace the all_paratax_df since dplyr doesn't play nice in for-loops
 # (I couldn't assign to a filtered object)
@@ -100,9 +95,7 @@ subsamps <- pinned_df %>%
 para_new <- all_paratax_by_ind %>% 
   filter(!(subsampleID %in% subsamps)) 
 for (id in subsamps) {
-  temp_exp_df <- pinned_df %>%
-    left_join(expert_df %>% 
-                dplyr::select(individualID, exp_sciname = scientificName)) %>%
+  temp_exp_df <- expert_pinned_df %>%
     dplyr::select(subsampleID, exp_sciname) %>%
     filter(!is.na(exp_sciname), subsampleID == id)
   temp_para_df <- all_paratax_by_ind %>% 
@@ -119,12 +112,9 @@ for (id in subsamps) {
 }
 
 ## Sanity check
-assertthat::assert_that(sum(!is.na(para_new$exp_sciname)) == nrow(expert_df))
+assertthat::assert_that(sum(!is.na(para_new$exp_sciname)) == nrow(expert_pinned_df))
 # Choose a subsample that has fewer expert IDs than sorting IDs: ta53RQUlpj5WhQksfJeD+QBAxMj6BQGBllkC8fUqt68=
-pinned_df %>%
-  left_join(expert_df %>% 
-              dplyr::select(individualID, exp_sciname = scientificName)) %>%
-  dplyr::select(subsampleID, exp_sciname) %>%
+expert_pinned_df %>%
   filter(!is.na(exp_sciname), subsampleID == "ta53RQUlpj5WhQksfJeD+QBAxMj6BQGBllkC8fUqt68=")
 para_new %>% filter(subsampleID == "ta53RQUlpj5WhQksfJeD+QBAxMj6BQGBllkC8fUqt68=")
 # We should see 6 jm of 7 individuals in para_new have an assigned exp_sciname
@@ -140,14 +130,16 @@ y_df <- y_df %>%
               group_by(plotID) %>%
               summarize(n=n()) %>%
               mutate(plotID_idx = 1:n()) %>%
-              dplyr::select(plotID, plotID_idx))
+              dplyr::select(plotID, plotID_idx)) %>%
+  mutate(parataxID_idx = match(y_df$parataxID, colnames),
+         expertID_idx = match(y_df$expertID, rownames))
 
 # Define L
 L <- reshape2::acast(para_new, plotID ~ collectDate ~ col_year)
 
 # Define alpha
-alpha <- matrix(1, nrow = length(rownames), ncol = length(colnames))
-diag(alpha) <- 10
+alpha <- matrix(1.5, nrow = length(rownames), ncol = length(colnames))
+diag(alpha) <- 100
 
 
 ## "Ground truth" data
@@ -156,19 +148,19 @@ diag(alpha) <- 10
 # We partly observe z
 z.dat <- array(NA, dim = c(length(unique(para_new$plotID)),
                            length(rownames),
-                           length(unique(all_paratax_df$col_year))),
+                           length(unique(all_paratax_by_ind$col_year))),
                dimnames = list(sort(unique(para_new$plotID)), #plots
                                rownames, #expert IDs
-                               unique(all_paratax_df$col_year)))
+                               unique(all_paratax_by_ind$col_year)))
 # Grab values from casted z.dat array and fill in values in final z.dat array
-z.dat_cast <- expert_df %>% 
+z.dat_cast <- expert_pinned_df %>% 
   mutate(occ = 1) %>%
-  reshape2::acast(plotID ~ scientificName ~ col_year,
+  reshape2::acast(plotID ~ exp_sciname ~ col_year,
                   fill=-999, drop=F, value.var = "occ")
 z.dat_cast[z.dat_cast == -999] <- NA
 z.dat_cast[z.dat_cast > 0] <- 1
 
-assertthat::assert_that(sum(z.dat_cast, na.rm=T) == nrow(expert_df %>% distinct(plotID, scientificName, col_year)))
+assertthat::assert_that(sum(z.dat_cast, na.rm=T) == nrow(expert_pinned_df %>% distinct(plotID, exp_sciname, col_year)))
 
 for(plot in dimnames(z.dat_cast)[[1]]) {
   for(spec in dimnames(z.dat_cast)[[2]]) {
@@ -214,24 +206,26 @@ jags_d <- list(nsite = dim(L)[1],
                site = y_df$plotID_idx, #needs to be numeric
                year = as.numeric(as.factor(y_df$col_year)), #needs to be numeric starting from 1
                # if the individual was labeled by the expert, true ID is known
-               k = y_df$expertID,
+               k = y_df$expertID_idx,
                # for all individuals, we get paratxonomist IDs
-               y = y_df$parataxID,
-               z = z.dat)
+               y = y_df$parataxID_idx,
+               z = z.dat,
+               R = diag(rep(1, 5)))
 JAGSinits <- function(){ list(z = z.init) }
-nc=4
+nc <- 4
+ni <- 80000
 cl <- makeCluster(nc)
 jm <- jags.parfit(cl = cl,
                   data = jags_d,
-                  params = c("psi", "p", "phi", "gamma", "lambda", "Theta",
+                  params = c("logit_psi", "logit_p", "log_phi", "logit_gamma",
+                             "log_lambda", "Theta","eps_site", "eps_spec",
                              "growth", "turnover"),
                   model = "occupancy/15.1_neon_dynamic_multisp_misclass_JAGS.txt",
-                  inits = JAGSinits,
                   n.chains = nc,
-                  n.adapt = 1000,
-                  n.update = 1000,
-                  thin = 1,
-                  n.iter = 10000)
+                  n.adapt = 5000,
+                  n.update = 5000,
+                  thin = ni/1000,
+                  n.iter = ni) 
 saveRDS(jm, "occupancy/script15.1_jags_jm.rds")
 jm_summ <- MCMCsummary(jm)
 saveRDS(jm_summ, "occupancy/script15.1_jm_mcmcsumm.rds")
@@ -249,12 +243,17 @@ range(jm_summ$Rhat, na.rm=TRUE)
 jm_summ <- rownames_to_column(jm_summ)
 jm_summ %>% filter(Rhat > 1.1)
 # year 2 and species 46 look sticky
+# rownames[46] is an expert ID, not ID'd by parataxonomist
 
-MCMCtrace(jm, params = paste0('growth\\[1,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
-MCMCtrace(jm, params = paste0('growth\\[3,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
-MCMCtrace(jm, params = paste0('growth\\[4,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
-MCMCtrace(jm, params = paste0('growth\\[7,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
-MCMCtrace(jm, params = paste0('growth\\[9,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('eps_site\\[1,5\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('eps_site\\[2,5\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('eps_site\\[11,5\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('eps_spec\\[1,5\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('eps_spec\\[2,5\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('eps_spec\\[11,5\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('growth\\[1,2,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('growth\\[2,6,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('growth\\[8,2,2\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
 
 # Look at raw numbers
 # theta
@@ -262,11 +261,14 @@ theta_summ <- MCMCsummary(jm, params = 'Theta', round=2)
 saveRDS(theta_summ, "occupancy/theta_summ_15.1.rds")
 theta_summ <- readRDS("occupancy/theta_summ_15.1.rds")
 hist(theta_summ$Rhat)
+hist(theta_summ$mean)
 range(theta_summ$Rhat, na.rm=TRUE)
 theta_summ <- rownames_to_column(theta_summ)
 
 # # Look at some theta posteriors with high Rhat values
-# MCMCtrace(jm, params = paste0('theta\\[5,5\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+MCMCtrace(jm, params = paste0('Theta\\[1,1\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
+theta_prior <- MCMCpack::rdirichlet(1000, c(100,rep(1.5,dim(alpha)[2]-1) ))
+plot(density(theta_prior[, 1]))
 # left_join(expert_df %>% select(individualID, expert=scientificName), pinned_df %>% select(individualID, para = scientificName)) %>% filter(scientificName==rownames[10])
 # MCMCtrace(jm, params = paste0('theta\\[5,16\\]'), type = 'both', ind = F, pdf=F, ISB=F, Rhat=T) 
 # MCMCsummary(jm, params='theta\\[36,[0-9]+\\]', ISB=F)
@@ -282,7 +284,7 @@ theta_df$para_morph = factor(theta_df$para_morph, levels=colnames)
 # Plot heatmap of theta values
 ggplot(theta_df, aes(x=para_morph, y=expert_sciname, fill= theta_mean)) + 
     geom_tile() +
-    scale_fill_gradient(limits = c(0,1), low="darkblue", high="white") +
+    scale_fill_gradient(limits=c(0,1),low="darkblue", high="white") +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
     scale_y_discrete(limits = rev(levels(as.factor(theta_df$expert_sciname))))
 #consider on plotly: https://www.r-graph-gallery.com/79-levelplot-with-ggplot2.html
