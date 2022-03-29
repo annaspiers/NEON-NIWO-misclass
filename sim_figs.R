975d69ce7a8ca47b8d0be76c62a3b9925fb01830
Anna Spiers <anna@melb2.colorado.edu>
2022-03-29 13:09:02 -0600

    generate more summary tables

--- sim_figs.R
+++ sim_figs.R
@@ -14,15 +14,15 @@ sims_jm_list <- lapply(temp, readRDS) #takes 1 min
 sims_jm <- rbindlist(sims_jm_list)
 rm(temp, sims_jm_list)
 
-# remaining <- setdiff(1:max(sims_jm$dataset),sims_jm$dataset) 
+# remaining <- setdiff(1:max(sims_jm$dataset),sims_jm$dataset)
 
-# Create new columns for plotting 
+# Create new columns for plotting
 # binary column specifying whether estimate 95% CI captures true value
 # numeric column specifying how far estimate median is from true value
 sims_jm_plot <- sims_jm %>%
-    mutate(capt95 = ifelse(true<`2.5%`,0,ifelse(true>`97.5%`,0,1)),
-           diff = `50%` - true, # positive means estimated median is greater than true value
-           ci95_width = `97.5%` - `2.5%`) # 95% CI width
+  mutate(capt95 = ifelse(true<`2.5%`,0,ifelse(true>`97.5%`,0,1)),
+         diff = `50%` - true, # positive means estimated median is greater than true value
+         ci95_width = `97.5%` - `2.5%`) # 95% CI width
 rm(sims_jm)
 
 # EDA
@@ -30,7 +30,7 @@ range(sims_jm$dataset) #number of iterations
 length(unique(sims_jm$dataset))
 sims_jm %>% distinct(model) #full vs reduced
 sims_jm %>% distinct(fraction) #by various fractions of validation
-sims_jm %>% group_by(param,model) %>% summarize(n=n()) 
+sims_jm %>% group_by(param,model) %>% summarize(n=n())
 sims_jm %>% group_by(param,model,fraction) %>% summarize(n=n()) %>% filter(param=="Theta") #why is n=60 for lambda/psi? 2species x 30 sites
 
 # Plot results ------------------------------------------------------------
@@ -40,79 +40,104 @@ sims_jm %>% group_by(param,model,fraction) %>% summarize(n=n()) %>% filter(param
 # AIS Brett says I may need to do a million reps?
 # Table
 sims_jm_plot %>%
-    group_by(param, fraction, model) %>%
-    summarize(capt_by95CI = sum(capt95)/n()) %>%
-    pivot_wider(names_from=fraction, values_from=capt_by95CI)
+  group_by(param, fraction, model) %>%
+  summarize(capt_by95CI = sum(capt95)/n()) %>%
+  pivot_wider(names_from=fraction, values_from=capt_by95CI)
 sims_jm_plot %>%
-    filter(param=="Theta") %>%
-    group_by(param, fraction, model) %>%
-    summarize(capt_by95CI = sum(capt95)/n()) %>%
-    group_by(param, model, capt_by95CI) %>%
-    summarize(n=n()) #few capt_by95CI values because there are only 6 %ages possible for coverage
+  filter(param=="Theta") %>%
+  group_by(param, fraction, model) %>%
+  summarize(capt_by95CI = sum(capt95)/n(),
+            capt_by95CI_alt = sum(capt95)/length(capt95))
+sims_jm_plot %>% filter(param=="Theta") %>%
+  group_by(param, fraction, model) %>%
+  summarize(capt_by95CI = sum(capt95)/n()) %>% head()
+sims_jm_plot %>% filter(param=="psi") %>%
+  mutate(rounded = round(fraction,digits=2) ) %>%
+  group_by(rounded) %>%
+  summarize(capt_by95CI = sum(capt95)/n()) %>%
+  ggplot() +
+  geom_point(aes(x=rounded,y=capt_by95CI))
+sims_jm_plot %>%
+  filter(param=="Theta") %>%
+  group_by(param, fraction, model) %>%
+  summarize(capt_by95CI = sum(capt95)/n()) %>%
+  group_by(param, model, capt_by95CI) %>%
+  summarize(n=n()) #few capt_by95CI values because there are only 6 %ages possible for coverage
 # since Theta has 6 different combos (2real species x 3 imperfect species)
 # Plot
 sims_jm_plot %>%
-    group_by(param, fraction, dataset, model) %>%
-    summarize(capt_by95CI = sum(capt95)/n()) %>%
-    mutate(param_model = paste0(param,"_",model)) %>%
-    #filter(capt_by95CI != 1) %>%
-    ggplot(aes(x=fraction, y=capt_by95CI)) + 
-    geom_point(size=0.3,alpha=0.2) +
-    geom_smooth() +
-    facet_wrap(vars(param_model)) +
-    labs(y="% of Estimates within 95% CI", x="Proportion of data validated")
+  filter(param=="psi") %>%
+  group_by(param, fraction, dataset, model) %>%
+  summarize(capt_by95CI = sum(capt95)/n()) %>%
+  mutate(param_model = paste0(param,"_",model)) %>%
+  #filter(capt_by95CI != 1) %>%
+  ggplot(aes(x=fraction, y=capt_by95CI)) +
+  geom_point(size=0.3,alpha=0.2)
+stat_summary(aes(y =capt_by95CI,group=1), fun.y=mean, colour="red", geom="line",group=1)
+facet_wrap(vars(param_model)) +
+  labs(y="% of Estimates within 95% CI", x="Proportion of data validated")
 ggsave("figures/simulations/coverage.png")
 # Just Theta plot
 sims_jm_plot %>%
-    filter(param=="Theta") %>%
-    group_by(param, fraction, dataset, model) %>%
-    summarize(capt_by95CI = sum(capt95)/n()) %>%
-    mutate(param_model = paste0(param,"_",model)) %>%
-    filter(capt_by95CI != 1) %>%
-    ggplot(aes(x=fraction, y=capt_by95CI)) + 
-    geom_point(size=0.1,alpha=0.5) +
-    geom_smooth() +
-    facet_wrap(vars(param_model)) +
-    labs(y="% of Estimates within 95% CI", x="Proportion of data validated")
+  filter(param=="Theta") %>%
+  group_by(param, fraction, dataset, model) %>%
+  summarize(capt_by95CI = sum(capt95)/n()) %>%
+  mutate(param_model = paste0(param,"_",model)) %>%
+  #filter(capt_by95CI != 1) %>%
+  ggplot(aes(x=fraction, y=capt_by95CI)) +
+  geom_point(size=0.1,alpha=0.5) +
+  geom_smooth() +
+  facet_wrap(vars(param_model)) +
+  labs(y="% of Estimates within 95% CI", x="Proportion of data validated")
 ggsave("figures/simulations/coverage_theta.png")
 
 
 # 2) Distance from true value
 # Difference between estimate median and true value
+# Theta
 sims_jm_plot %>%
-    filter(param=="Theta") %>%
-    head(1000) %>%
-    ggplot(aes(x=fraction, y=abs(diff))) + 
-    geom_point(size=0.1,alpha=0.3) +
-    #geom_smooth() +
-    facet_grid(cols=vars(model)) +
-    labs(y="Estimated Median - True Value", x="Proportion of data validated")
+  filter(param=="Theta") %>%
+  head(1000) %>%
+  ggplot(aes(x=fraction, y=abs(diff),col=model)) +
+  geom_point(size=0.1,alpha=0.3) +
+  geom_smooth() +
+  #facet_grid(cols=vars(model)) +
+  labs(y="| Estimated Median - True Value |", x="Proportion of data validated")
 ggsave("figures/simulations/dist_from_true_value_theta.png")
-
+# Psi
 sims_jm_plot %>%
-    filter(param=="psi") %>%
-    head(5000000) %>%
-    ggplot(aes(x=fraction, y=abs(diff))) + 
-    geom_point(size=0.1,alpha=0.01) +
-    geom_smooth() +
-    labs(y="Estimated Median - True Value", x="Proportion of data validated")
+  filter(param=="psi") %>%
+  #head(5000000) %>%
+  ggplot(aes(x=fraction, y=abs(diff))) +
+  geom_point(size=0.1,alpha=0.01) +
+  geom_smooth() +
+  labs(y="Estimated Median - True Value", x="Proportion of data validated")
 ggsave("figures/simulations/dist_from_true_value_psi.png")
-
+# Lambda
 sims_jm_plot %>%
-    filter(param=="lambda") %>%
-    ggplot(aes(x=fraction, y=abs(diff))) + 
-    geom_point(size=0.1,alpha=0.01) +
-    geom_smooth() +
-    facet_grid(cols=vars(model)) +
-    labs(y="Estimated Median - True Value", x="Proportion of data validated")
+  filter(param=="lambda") %>%
+  ggplot(aes(x=fraction, y=abs(diff))) +
+  geom_point(size=0.1,alpha=0.01) +
+  geom_smooth() +
+  facet_grid(cols=vars(model)) +
+  labs(y="Estimated Median - True Value", x="Proportion of data validated")
 ggsave("figures/simulations/dist_from_true_value_lambda.png")
+# All
+sims_jm_plot %>%
+  filter(dataset < 100) %>%
+  mutate(param_model = paste0(param,"_",model)) %>%
+  ggplot(aes(x=fraction, y=abs(diff))) +
+  geom_point(size=0.1,alpha=0.3) +
+  geom_smooth() +
+  facet_grid(rows=vars(param_model),scales="free") +
+  labs(y="| Estimated Median - True Value |", x="Proportion of data verified")
+ggsave("figures/simulations/dist_from_true_value_all.png")
 
 
 # 3) Plot proportion of data labeled vs 95% CI width - plot full vs reduced model
 # Theta
 sims_jm_plot %>%
     filter(param=="Theta") %>% 
-    #head(100) %>%
     mutate(param_name = paste0(param,"[",i,",",j,"]")) %>%
     ggplot(aes(x=fraction, y=ci95_width, col=model)) +
     geom_point(size=.3, alpha=0.05, position = "jitter") +
@@ -121,12 +146,13 @@ sims_jm_plot %>%
     labs(x="Proportion of data labeled", y="95% CI width")
 ggsave("figures/simulations/95CIwidth_theta.png")
 # Psi
-sims_jm_plot %>%
+system.time(sims_jm_plot %>%
     filter(param=="psi") %>% #filter(param=="psi" & fraction==pull(sims_jm_plot[1,"fraction"])) %>% 
+    head(50000000) %>%
     ggplot(aes(x=fraction, y=ci95_width)) +
     geom_point(size=.5, alpha=0.05) +
     geom_smooth()  +
-    labs(x="Proportion of data labeled", y="95% CI width",title="Psi")
+    labs(x="Proportion of data labeled", y="95% CI width",title="Psi"))
 ggsave("figures/simulations/95CIwidth_psi.png")
 # Lambda
 sims_jm_plot %>%
@@ -136,7 +162,29 @@ sims_jm_plot %>%
     geom_smooth()  +
     labs(x="Proportion of data labeled", y="95% CI width",title="Lambda")
 ggsave("figures/simulations/95CIwidth_lambda.png")
+# All
+sims_jm_plot %>%
+  filter(dataset< 100) %>% 
+  mutate(param_model = paste0(param,"_",model)) %>%
+  ggplot(aes(x=fraction, y=ci95_width)) +
+  geom_point(size=.5, alpha=0.05) +
+  geom_smooth()  +
+  facet_grid(rows=vars(param_model),scales="free") +
+  labs(x="Proportion of data labeled", y="95% CI width")
+ggsave("figures/simulations/95CIwidth_all.png")
 
+# #scrap
+# scrap_psi <- sims_jm_plot %>% 
+#     filter(param=="psi") %>% 
+#     mutate(rounded = round(fraction,digits=2) ) %>% 
+#     group_by(rounded) %>% 
+#     summarize(mean=mean(ci95_width), 
+#               sd=sd(ci95_width), 
+#               med=median(ci95_width)) 
+# scrap_psi %>%
+#     ggplot() +
+#     geom_point(aes(x=rounded,y=mean))
+# ggsave("figures/simulations/95CIwidth_lambda.png")
 
 
 
@@ -147,10 +195,10 @@ sims_jm_plot %>%
     pivot_wider(names_from=model, values_from=ci95_width) %>%
     ggplot(aes(x=reduced, y=full)) +
     geom_point(aes(col=fraction),size=0.5) +
-    scale_color_viridis(alpha=0.6) +
+    #scale_color_viridis(alpha=0.6) +
     geom_abline(aes(intercept=0, slope=1)) +
     coord_cartesian(xlim=c(0,.08), ylim=c(0,.08)) +
-    labs(x="misclassification only 95% CI width", y="occupancy-classification 95% CI width",
+    labs(x="Classification-only 95% CI width", y="Classification-occupancy 95% CI width",
          col="Fraction Validated") 
 ggsave("figures/simulations/95CI_fullvred.png")
 
@@ -190,7 +238,7 @@ sims_val %>%
     geom_point(size=0.5,alpha=0.02) +
     stat_smooth() +
     facet_grid(cols=vars(metric)) +
-    labs(x="Proportion of data labeled", y="Metric Value")
+    labs(x="Proportion of data verified", y="Validation Metric Value")
 ggsave("figures/simulations/val_all.png")
 # Accuracy
 sims_val %>%
